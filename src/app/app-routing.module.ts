8fb61655ca031a90bd76e43f51a2bbe4f1901189
Sebastian Malcher <sebastianlukasmalcher@gmail.com>
2020-08-05 15:35:36 +0200

    Adds sign up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SuppliersComponent } from './suppliers/suppliers.component';
 import { AboutComponent } from './about/about.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserSignUpComponent } from './sign-up/user-sign-up/user-sign-up.component';
+import { SupplierSignUpComponent } from './sign-up/supplier-sign-up/supplier-sign-up.component';
 
 
 const routes: Routes = [
   {path: '', component: SuppliersComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'signup', component: SignUpComponent, children: [{path: 'user', component: UserSignUpComponent}, {path: 'supplier', component: SupplierSignUpComponent}]}
 ];
 
 @NgModule({
