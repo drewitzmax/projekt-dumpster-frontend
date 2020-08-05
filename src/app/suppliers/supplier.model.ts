e87d5332c2508112954c69f1d17847f73181d55b
grizzledizzle <vinzig@ymail.com>
2020-08-05 14:10:46 +0200

    added static dummy data, iframe

--- src/app/suppliers/supplier.model.ts
+++ src/app/suppliers/supplier.model.ts
@@ -1,3 +1,4 @@
 export class Supplier {
-    constructor(public name: string, public type: string, public imgPath: string) {}
+    constructor(public id: number ,public name: string,public address: string, public phoneNumber: string,
+                public email: string, public password: string, public homepageUrl: string, public photos: string) {}
 }
--- src/app/suppliers/suppliers.service.ts
+++ src/app/suppliers/suppliers.service.ts
@@ -3,11 +3,14 @@ import { Supplier } from './supplier.model'
 
 export class SuppliersService{
     private suppliers: Supplier[] = [
-        new Supplier("Pizza Mari", "Pizzeria","https://www.fressfreunde.at/wp-content/uploads/2019/09/Mari_Header.jpg"),
-        new Supplier("Alberto Schnitzel", "Österreichisch","https://www.gutekueche.at/img/rezept/170/460x307_wiener-schnitzel.jpg")
+        new Supplier(1,"Pizza Mari","Leopoldsgasse 23A, 1020 Wien","012345678","pizza@mari.at","1234","https://www.pizzamari.at/",
+        "https://www.fressfreunde.at/wp-content/uploads/2019/09/Mari_Header.jpg"),
+        
+        new Supplier(2,"Alberto Schnitzel","Albertogaße 1, 1110, Wien","07654321","alberto@schnitzl.at","4321","https://www.pizzamari.at/",
+        "https://www.gutekueche.at/img/rezept/170/460x307_wiener-schnitzel.jpg")
     ];
 
     getSuppliers() {
-        return this.suppliers;
+        return this.suppliers.slice();
     }
 }
