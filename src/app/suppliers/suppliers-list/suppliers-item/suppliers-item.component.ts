9ccacd76600082eb7ff6e836384ef082147312d6
grizzledizzle <vinzig@ymail.com>
2020-08-05 15:25:56 +0200

    get-request handled in supplier.service

--- src/app/suppliers/suppliers-list/suppliers-item/suppliers-item.component.ts
+++ src/app/suppliers/suppliers-list/suppliers-item/suppliers-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SuppliersService } from '../../suppliers.service';
 import { Supplier } from '../../supplier.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-suppliers-item',
@@ -13,10 +14,19 @@ export class SuppliersItemComponent implements OnInit {
   suppliers: Supplier[];
   hideBody: boolean = true;
 
-  constructor(private supService: SuppliersService) { }
+  constructor(private supService: SuppliersService,private http: HttpClient) {
+/*     this.http.get(this.url + "/provider").toPromise().then(
+      data => {console.log(data);
+      
+      for( let item in data) {
+        if(data.hasOwnProperty(item)){
+          this.items.push(data[item])
+        }
+      }
+    }) */
+   }
 
   ngOnInit() {
-
     this.suppliers = this.supService.getSuppliers();    //Mit ng-bootstrap
   }
 
--- src/app/suppliers/suppliers.service.ts
+++ src/app/suppliers/suppliers.service.ts
@@ -1,16 +1,44 @@
 import { Injectable } from '@angular/core';
 import { Supplier } from './supplier.model'
+import { HttpClient } from '@angular/common/http';
 
+@Injectable()
 export class SuppliersService{
-    private suppliers: Supplier[] = [
+    url = "http://localhost:8080"
+    items = [];
+
+/*     private suppliers: Supplier[] = [
         new Supplier(1,"Pizza Mari","Leopoldsgasse 23A, 1020 Wien","012345678","pizza@mari.at","1234","https://www.pizzamari.at/",
         "https://www.fressfreunde.at/wp-content/uploads/2019/09/Mari_Header.jpg"),
         
         new Supplier(2,"Alberto Schnitzel","Albertogaße 1, 1110, Wien","07654321","alberto@schnitzl.at","4321","https://www.pizzamari.at/",
         "https://www.gutekueche.at/img/rezept/170/460x307_wiener-schnitzel.jpg")
+    ]; */
+
+    private suppliers: Supplier[] = [
+
     ];
 
+    constructor(private http: HttpClient) {
+        this.http.get(this.url + "/provider").toPromise().then(
+          data => {console.log(data);
+          
+          for( let item in data) {
+            if(data.hasOwnProperty(item)){
+                let prov = data[item];
+                let newSupplier = new Supplier(prov.id, prov.name, prov.address, prov.phoneNumber,
+                    prov.email, prov.password, prov.homepageUrl, prov.photos);
+                
+                this.suppliers.push(newSupplier);
+              
+                console.log(newSupplier.address)
+            }
+          }
+        })
+       }
+    
+
     getSuppliers() {
-        return this.suppliers.slice();
+        return this.suppliers;
     }
 }
