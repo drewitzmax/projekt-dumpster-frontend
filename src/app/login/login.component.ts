0e4b03120053f736d1f73b34879c0ab8de84d2de
grizzledizzle <vinzig@ymail.com>
2020-08-06 17:26:31 +0200

    error in get request

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  url = "http://localhost:8080";
+
+  
+  constructor(private http: HttpClient) {
+
+   }
+  submitted: boolean;
+
+  loginForm = new FormGroup({
+    username : new FormControl('',Validators.required),
+    password : new FormControl('',Validators.required)
+  });
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.http.get(this.url + "/login").subscribe(
+      data => {console.log(data);
+    })
+    
+    this.submitted = true;
+    console.log("submitted");
+
   }
 
 }
