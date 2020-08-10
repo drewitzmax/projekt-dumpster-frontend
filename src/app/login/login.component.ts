24ff992456346835a3a48db645308b1a857bc0a6
grizzledizzle <vinzig@ymail.com>
2020-08-10 14:08:37 +0200

    added login data to local storage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { LoginService } from './login.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -12,10 +13,14 @@ export class LoginComponent implements OnInit {
 
 
   submitted: boolean;
-  greeting = {};
 
   
   constructor(private loginService: LoginService) {
+    if (this.loginService.currentUserValue) { 
+      console.log(`Logged in as: ${this.loginService.currentUserValue}`)
+    }else {
+      console.log("Not logged in")
+    }
 
    }
 
@@ -34,12 +39,17 @@ export class LoginComponent implements OnInit {
     let password = this.loginForm.controls.password.value;
     
     let resp = this.loginService.login(username,password);
-    resp.subscribe(data=>{
+    resp
+    .pipe(first())
+    .subscribe(data=>{
       console.log(data);
     })
-    
-
-
   }
+
+  onLogout() {
+    this.loginService.logout();
+  }
+
+  
 
 }
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,19 +1,57 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { catchError, retry, map } from 'rxjs/operators';
+import { throwError, BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  authorized: boolean;
+  authenticated= false;
   url = "http://localhost:8080";
 
-  constructor(private http: HttpClient) { }
+  private currentUserSubject: BehaviorSubject<any>;
+  public currentUser: Observable<any>;
 
-  login(username: string, password: string){
 
-    const headers = new HttpHeaders({Authorization: 'Basic '+btoa(username+":"+password)})
+  constructor(private http: HttpClient) { 
+    this.currentUserSubject = new BehaviorSubject<any>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
-    return this.http.get(this.url + "/login",{headers, responseType: 'text' as 'json'})
+  public get currentUserValue() {
+    return this.currentUserSubject.value;
+}
+
+  login(username: string, password: string) {
+
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ":" + password) })
+
+    return this.http.get(this.url + "/login", { headers, responseType: 'text' as 'json' })
+    .pipe(map(user => {
+      
+      localStorage.setItem('currentUser', JSON.stringify(user));
+      this.currentUserSubject.next(user);
+      return user;
+    }));
+
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+
+  private handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
   }
 }
