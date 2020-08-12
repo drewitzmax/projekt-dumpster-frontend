b12136d865629ec69807cdcc4d85db8c81a924db
grizzledizzle <vinzig@ymail.com>
2020-08-12 13:29:16 +0200

    form control ok- positive feedback missing

--- src/app/offers/place-offer/place-offer.component.ts
+++ src/app/offers/place-offer/place-offer.component.ts
@@ -20,16 +20,16 @@ export class PlaceOfferComponent implements OnInit {
   ngOnInit(): void {
   }
   onSubmit() {
-    let newOffer = new Offer(null,
-      this.form.get("title").value, 
-      null,
-      this.form.get("amountOffered").value,
-      this.form.get("amountOffered").value);
-
-    console.log(typeof this.form.get("amountOffered").value+" amountOffered")
-    console.log(typeof newOffer.amountOffered+" "+newOffer.amountOffered+" newoffer");
-    this.offerService.placeOffer(newOffer)
-      .subscribe(offer => console.log(offer));
+    this.submitted = true;
+    if(this.form.valid){
+      let newOffer = new Offer(null,
+                                this.form.get("title").value, 
+                                null,
+                                this.form.get("amountOffered").value,
+                                this.form.get("amountOffered").value);
+  
+      this.offerService.placeOffer(newOffer).subscribe(offer => console.log(offer));
+    }
   }
 
 }
