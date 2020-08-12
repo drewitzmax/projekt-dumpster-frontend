583a2855951d130ece5edd8bae677bd88c4a5126
grizzledizzle <vinzig@ymail.com>
2020-08-12 09:42:27 +0200

    added place-offer basic func,  tweaked login

--- src/app/offers/offers.service.ts
+++ src/app/offers/offers.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject, ComponentFactoryResolver } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Offer } from './offer.model';
 import { catchError } from 'rxjs/internal/operators/catchError';
 import { User } from '../users/user.model';
@@ -14,15 +14,33 @@ export class OffersService {
   offerUrl = "http://localhost:8080/offer";
   factoryResolver: any;
   rootViewContainer: any;
+  private userRole: string;
+  private username: string;
+  private password: string;
 
-
+  
+  
   constructor(private http: HttpClient, @Inject(ComponentFactoryResolver) factoryResolver) {
-    this.factoryResolver = factoryResolver }
+    this.userRole = localStorage.getItem('currentUserRole');
+    this.username = localStorage.getItem('currentUserName');
+    this.password = localStorage.getItem('currentUserPassword');
+    this.factoryResolver = factoryResolver 
+  }
 
  public getOffers() {
     return this.http.get(this.offerUrl)
       .pipe(
         catchError(this.handleError('getOffers'))
+      );
+
+  }
+
+  placeOffer(offer: Offer): Observable<Offer> {
+    
+    let headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(this.username + ":" + this.password) })
+    return this.http.post<Offer>(this.offerUrl, offer,{ headers, responseType: 'text' as 'json' })
+      .pipe(
+        catchError(this.handleError('addOffer', offer))
       );
 
   }
